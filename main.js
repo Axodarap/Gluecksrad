0f56332158cd7a5a6706aa1622d439c4b88f2d26
Axodarap <jakob.soukup@aon.at>
2025-08-16 17:57:45 +0200

    added basic wheel logic

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-/*import { Wheel } from "https://cdn.jsdelivr.net/npm/spin-wheel@4.3.0/dist/spin-wheel-esm.js";*/
 
+
+let items = [];
 
 const wheelContainer = document.getElementById("wheel-container");
 const wheelProps = {
-  items: [
-    { label: 'one' },
-    { label: 'two' },
-    { label: 'three' },
-  ],
+  items: items,
   itemBackgroundColors: ['#fff', '#6e0f0fff', '#2d2baaff'],
   itemLabelFontSizeMax: 40,
   rotationResistance: -100,
@@ -17,11 +14,43 @@ const wheelProps = {
 
 var wheel = new spinWheel.Wheel(wheelContainer, wheelProps);
 
-
-function clicked() {
-  
-  wheelProps.items.push({ label: 'test' }); // add a new item
-  wheel.init(wheelProps)
-  
+function pickItem(){
+  const winningIndex = Math.floor(Math.random() * items.length);
+  const duration = 10000;
+  wheel.spinToItem(winningIndex, duration, true, 4, 1, easeOutQuad);
+  let selItem = items[winningIndex];
+  console.log(selItem.label);
 }
 
+/**
+ * Adds a new item to the wheel.
+ */
+function addItem(item){
+  items.push({label: item})
+  wheelProps.items = items;
+  wheel.init(wheelProps);
+}
+
+/**
+ * Removes an item from the wheel.
+ */
+function removeItem(index) {
+  wheelProps.items.splice(index, 1);
+  wheel.init(wheelProps);
+}
+
+
+/* easing functions TODO: move to module */
+function easeOutExpo( t ) {
+
+    if( t === 1 ) {
+        return 1;
+    }
+
+    return ( -Math.pow( 2, -10 * t ) + 1 );
+
+}
+
+function easeOutQuad( t ) {
+    return t * ( 2 - t );
+}
